dfebc0e72cc0c5690bea000470b335c60db7d311
Ebs012 <134578012+Ebs012@users.noreply.github.com>
2023-08-26 17:34:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -138,16 +138,14 @@ resource "aws_lb_target_group" "web_target_group" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.Terra-vpc.id
-  target_type = "instance"
 
   health_check {
     enabled              = true
+    interval            = 30
+    path                = "/health.html"
+    timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout             = 3
-    interval            = 30
-    path                = "/health.html" 
-    #target              = "HTTP:80/"
   }
 
 }
